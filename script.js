a56e8121c6313e487844bbfc2caed8b65fdedf33
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-11-01 11:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-// complete this js code
-function Person(name, age) {}
+// Person class
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Employee class inheriting from Person
+function Employee(name, age, jobTitle) {
+  // call the parent constructor
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+// inherit from Person prototype
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+// method for Employee
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
